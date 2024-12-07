b8a2f60ae3d6e372cf5c88d4ab77246a27abfdb3
Alex-3454 <notalexnokia@gmail.com>
2024-12-07 14:35:28 +0000

    Add files via upload

---
+++ external.js
@@ -0,0 +1,13 @@
+<!--Random causes for effects of climate change.-->
+
+
+function arrayTwo(){
+	//declare the Array
+	<!--types-->
+	let X=["Hotter temperatures", "More severe storms", "Increased drought", "A warming, rising ocean", "Loss of species", "Not enough food", "More health risks", "Poverty and displacement"];
+	let doc= document.getElementById("task2");
+	<!--Max 7 options can change if needed-->
+	doc.innerHTML = "- "+ X[Math.floor(Math.random()*7)];
+	l
+
+}
